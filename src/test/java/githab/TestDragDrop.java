2e906e68b681f65f3ae6e77426b47653715c1bc3
evgen-kaz <itroute@mail.ru>
2025-08-19 14:40:33 +0800

    add commit

---
+++ src/test/java/githab/TestDragDrop.java
@@ -0,0 +1,39 @@
+package githab;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestDragDrop {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        baseUrl = "https://the-internet.herokuapp.com/";
+    }
+
+    @Test
+    void droppingWithActionTest() {
+        open("/drag_and_drop");
+        SelenideElement leftSquare = $("#column-a").shouldHave(text("A"));
+        SelenideElement rightSquare = $("#column-b").shouldHave(text("B"));
+        actions().clickAndHold(leftSquare).moveToElement(rightSquare).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void droppingWithDragAndDropTest() {
+        open("/drag_and_drop");
+        SelenideElement leftSquare = $("#column-a").shouldHave(text("A"));
+        SelenideElement rightSquare = $("#column-b").shouldHave(text("B"));
+        leftSquare.dragAndDrop(to(rightSquare));
+        leftSquare.shouldHave(text("B"));
+        rightSquare.shouldHave(text("A"));
+    }
+}
---
+++ src/test/java/githab/TestsGithub.java
@@ -0,0 +1,34 @@
+package githab;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.withTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TestsGithub {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        baseUrl = "https://github.com/";
+    }
+
+    @Test
+    void testsGithub() {
+        String textOnPageEnterprises = """
+                The AI-powered
+                developer platform
+                To build, scale, and deliver secure software.
+                """;
+
+        open(baseUrl);
+        $(".HeaderMenu-wrapper").$(withTagAndText("button", "Solutions")).hover();
+        $(withTagAndText("a", "Enterprises")).click();
+        $("div").shouldHave(text(textOnPageEnterprises));
+    }
+}
